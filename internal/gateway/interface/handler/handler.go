944c50c194c81997b1b229c3bbbdfb72db433b72
Shouting <z9905080@gmail.com>
2023-06-06 23:57:02 +0800

    [init] create init version-1

--- internal/gateway/interface/handler/handler.go
+++ internal/gateway/interface/handler/handler.go
@@ -3,13 +3,13 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/z9905080/melody"
-	usecase2 "slot-framework/internal/gateway/usecase"
+	usecase "slot-framework/internal/gateway/usecase"
 )
 
 type Handler struct {
 	App          *gin.Engine
 	WebsocketApp *melody.Melody
-	GatewaySrv   usecase2.InfGateway
+	GatewaySrv   usecase.InfGateway
 }
 
 func (h *Handler) SetWebsocketRouter() {
@@ -29,7 +29,7 @@ func (h *Handler) SetWebsocketRouter() {
 func (h *Handler) ClientRequestHandler(s *melody.Session, msg []byte) {
 	funcMap := h.getServerFuncMap()
 	if fn, isExist := funcMap["game_flow"]; isExist {
-		fn(usecase2.CmdOperation{
+		fn(usecase.CmdOperation{
 			Server:  h.WebsocketApp,
 			Session: s,
 			Data:    string(msg),
@@ -37,8 +37,8 @@ func (h *Handler) ClientRequestHandler(s *melody.Session, msg []byte) {
 	}
 }
 
-func (h *Handler) getServerFuncMap() map[string]func(flow usecase2.CmdOperation) (usecase2.EventOperation, error) {
-	return map[string]func(usecase2.CmdOperation) (usecase2.EventOperation, error){
+func (h *Handler) getServerFuncMap() map[string]func(flow usecase.CmdOperation) (usecase.EventOperation, error) {
+	return map[string]func(usecase.CmdOperation) (usecase.EventOperation, error){
 		"game_flow": h.GatewaySrv.GameFlow,
 	}
 }
